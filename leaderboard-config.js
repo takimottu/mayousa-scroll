63c1428cfd485e0e81779f26038203af772614a8
takimottu <takimottu@gmail.com>
2026-08-07 20:24:51 +0900

    Update leaderboard-config.js

--- leaderboard-config.js
+++ leaderboard-config.js
@@ -1,5 +1,5 @@
 window.MAYOUSA_LEADERBOARD = {
-  supabaseUrl: "",
-  supabaseAnonKey: "",
+  supabaseUrl: "https://izffgchvhwkngbqgbhsh.supabase.co",
+  supabaseAnonKey: "sb_publishable_PSAm6DcFM2DSVCY3rZkwnQ_6jEE_8JG",
   table: "mayousa_scores",
 };
